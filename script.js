7cd231e5ff74c6c8ea05847c0b4dfeee8670978b
ShreyanshPandey73 <117717418+ShreyanshPandey73@users.noreply.github.com>
2023-04-14 20:29:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+ $('#evaluatedText').on('input', function() {
+            // Get the value of the input text
+            const text = $(this).val();
+            // Get the length of the text
+            const count = text.length;
+            // Update the letter count in the h3 element
+            $('#letterCount').text(count);
+        });
